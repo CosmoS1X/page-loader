bbdd490fb0601b11eb7952c9c4dfb02df990c627
Konstantin Korovin <cosmo.stan@yandex.com>
2024-08-29 22:18:48 +0300

    feat: implement saving HTML page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-export default () => {
-  const greeting = 'Hello from new repository';
-  console.log(greeting);
+import axios from 'axios';
+import fsp from 'fs/promises';
+import path from 'path';
 
-  return greeting;
+const fetchPageData = (url) => axios.get(url)
+  .then((response) => response.data)
+  .catch((error) => {
+    throw error;
+  });
+
+const getFileNameFromUrl = (url) => {
+  const [, urlWithoutScheme] = url.split('://');
+  const filename = urlWithoutScheme.split(/[^a-zA-Z0-9]/).join('-').concat('.html');
+
+  return filename;
+};
+
+const savePage = (filepath, data) => {
+  fsp.writeFile(filepath, data);
+};
+
+export default (url, dirname) => {
+  const filename = getFileNameFromUrl(url);
+  const fullpath = path.join(dirname, filename);
+
+  return fetchPageData(url)
+    .then((data) => savePage(fullpath, data))
+    .then(() => fullpath);
 };
