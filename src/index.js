44ca64cac96ff03d06eb579949e130fd0a12e72c
Konstantin Korovin <cosmo.stan@yandex.com>
2024-09-04 19:35:43 +0300

    fix: fix saveImages func

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import fs from 'fs';
 import fsp from 'fs/promises';
 import path from 'path';
 import * as cheerio from 'cheerio';
@@ -14,24 +13,21 @@ const prettifyHTML = (html) => {
   return prettier.format(html, options);
 };
 
-const fetchHTML = (url) => axios.get(url)
+const fetchData = (url, options = {}) => axios.get(url, options)
   .then((response) => response.data)
   .catch((error) => {
     throw error;
   });
 
-const fetchImage = (url, output) => axios.get(url, { responseType: 'stream' })
-  .then((response) => response.data.pipe(fs.createWriteStream(output)));
+const saveFile = (output, data) => fsp.writeFile(output, data);
 
 const parseName = (name) => name.replace(/[^a-zA-Z0-9]/g, '-');
-
-const savePage = (filepath, html) => fsp.writeFile(filepath, html);
 
 const makeResourcesDir = (root, dirname) => (
   fsp.mkdir(path.join(root, dirname))
 );
 
-const saveImages = (root, hostname, filesDirName, htmlPath) => fsp.readFile(htmlPath, 'utf-8')
+const saveImages = (root, origin, hostname, filesDirName, htmlPath) => fsp.readFile(htmlPath, 'utf-8')
   .then((html) => {
     const $ = cheerio.load(html);
 
@@ -41,33 +37,31 @@ const saveImages = (root, hostname, filesDirName, htmlPath) => fsp.readFile(html
       const dirname = path.dirname(link);
       const basename = path.basename(link, extname);
       const imgName = parseName(path.join(hostname, dirname, basename)).concat(extname);
-      const imgPath = path.join(root, filesDirName, imgName);
+      const imgUrl = `${origin}${link}`;
+      const output = path.join(root, filesDirName, imgName);
 
-      // eslint-disable-next-line no-param-reassign
-      elem.attribs.src = path.join(filesDirName, imgName);
-
-      if (link.startsWith('http')) {
-        return fetchImage(link, imgPath);
+      if (!link.startsWith('https')) {
+        // eslint-disable-next-line no-param-reassign
+        elem.attribs.src = path.join(filesDirName, imgName);
+        return fetchData(imgUrl, { responseType: 'stream' })
+          .then((buffer) => saveFile(output, buffer));
       }
-
-      return fsp.readFile(path.join(process.cwd(), link))
-        .then((data) => fsp.writeFile(imgPath, data));
     });
 
     return Promise.resolve($.html());
   })
   .then((html) => prettifyHTML(html))
-  .then((html) => fsp.writeFile(htmlPath, html));
+  .then((html) => saveFile(htmlPath, html));
 
 export default (url, root) => {
-  const { hostname, pathname } = new URL(url);
+  const { origin, hostname, pathname } = new URL(url);
   const htmlName = parseName(path.join(hostname, pathname)).concat('.html');
   const resourcesDirName = parseName(path.join(hostname, pathname)).concat('_files');
   const htmlPath = path.join(root, htmlName);
 
-  return fetchHTML(url)
-    .then((html) => savePage(htmlPath, html))
+  return fetchData(url)
+    .then((html) => saveFile(htmlPath, html))
     .then(() => makeResourcesDir(root, resourcesDirName))
-    .then(() => saveImages(root, hostname, resourcesDirName, htmlPath))
+    .then(() => saveImages(root, origin, hostname, resourcesDirName, htmlPath))
     .then(() => htmlPath);
 };
