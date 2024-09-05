80971033d3787f24067ca0b4964e4816e4cdf504
Konstantin Korovin <cosmo.stan@yandex.com>
2024-09-05 17:32:30 +0300

    test: update tests for saving images

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,15 @@
 import path from 'path';
-import os from 'os';
+import { fileURLToPath } from 'url';
 import fsp from 'fs/promises';
 import nock from 'nock';
+import os from 'os';
 import app from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename, encoding = 'utf-8') => fsp.readFile(getFixturePath(filename), encoding);
 
 nock.disableNetConnect();
 
@@ -14,14 +21,21 @@ beforeEach(async () => {
 
 it('should return the fullpath of the loaded page', async () => {
   const url = 'https://ru.hexlet.io/courses';
-  const data = await fsp.readFile(path.join(process.cwd(), '__fixtures__', 'before.html'), 'utf-8');
+  const html = await readFile('before.html');
+  const img = await readFile('nodejs.png');
+
+  nock('https://ru.hexlet.io')
+    .get('/courses')
+    .reply(200, html);
+
+  nock('https://ru.hexlet.io')
+    .get('/assets/professions/nodejs.png')
+    .reply(200, img);
+
   const expected = path.join(tmpDir, 'ru-hexlet-io-courses.html');
+  const actual = await app(url, tmpDir);
 
-  nock(/ru\.hexlet\.io/)
-    .get(/\/courses/)
-    .reply(200, data);
-
-  await expect(app(url, tmpDir)).resolves.toBe(expected);
+  expect(actual).toBe(expected);
 });
 
 it('should save images', async () => {
@@ -29,29 +43,42 @@ it('should save images', async () => {
   const resourcesDir = path.join(tmpDir, 'ru-hexlet-io-courses_files');
   const imgName = 'ru-hexlet-io-assets-professions-nodejs.png';
   const imgFilePath = path.join(resourcesDir, imgName);
-  const data = await fsp.readFile(path.join(process.cwd(), '__fixtures__', 'before.html'), 'utf-8');
+  const html = await readFile('before.html');
+  const expected = await readFile('nodejs.png');
 
-  nock(/ru\.hexlet\.io/)
-    .get(/\/courses/)
-    .reply(200, data);
+  nock('https://ru.hexlet.io')
+    .get('/courses')
+    .reply(200, html);
+
+  nock('https://ru.hexlet.io')
+    .get('/assets/professions/nodejs.png')
+    .reply(200, expected);
 
   await app(url, tmpDir);
 
-  await expect(fsp.access(imgFilePath)).resolves.toBe(undefined);
+  const actual = await fsp.readFile(imgFilePath, 'utf-8');
+
+  expect(actual).toEqual(expected);
 });
 
 it('should change links in html file', async () => {
   const url = 'https://ru.hexlet.io/courses';
-  const data = await fsp.readFile(path.join(process.cwd(), '__fixtures__', 'before.html'), 'utf-8');
+  const html = await readFile('before.html');
+  const htmlFilePath = path.join(tmpDir, 'ru-hexlet-io-courses.html');
+  const img = await readFile('nodejs.png');
 
-  nock(/ru\.hexlet\.io/)
-    .get(/\/courses/)
-    .reply(200, data);
+  nock('https://ru.hexlet.io')
+    .get('/courses')
+    .reply(200, html);
+
+  nock('https://ru.hexlet.io')
+    .get('/assets/professions/nodejs.png')
+    .reply(200, img);
 
   await app(url, tmpDir);
 
-  const actual = await fsp.readFile(path.join(tmpDir, 'ru-hexlet-io-courses.html'), 'utf-8');
-  const expected = await fsp.readFile(path.join(process.cwd(), '__fixtures__', 'after.html'), 'utf-8');
+  const actual = await fsp.readFile(htmlFilePath, 'utf-8');
+  const expected = await readFile('after.html');
 
   expect(actual).toBe(expected);
 });
