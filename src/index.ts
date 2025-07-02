7c69fed06adc2dbcf3b8bdf104a9b8d244268125
Konstantin Korovin <cosmo.stan@yandex.com>
2025-07-02 19:55:28 +0300

    feat: infer types and rename files with .ts extension

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
 import fsp from 'fs/promises';
 import Listr from 'listr';
-import htmlParser from './htmlParser.js';
+import htmlParser from './htmlParser';
 import {
   fetchData,
   buildPath,
   prettifyHTML,
   sanitizeFileName,
   saveFile,
-} from './utils.js';
+} from './utils';
 
-const getMeta = (html, baseUrl, resourcesDirPath) => {
+const getMeta = (html: string, baseUrl: string, resourcesDirPath: string) => {
   const instance = htmlParser(html);
   const resourcesMeta = instance.processResources(baseUrl, resourcesDirPath);
 
   return { html: instance.getHTML(), resources: resourcesMeta };
 };
 
-const downloadResources = (html, baseUrl, resourcesDirPath) => {
+const downloadResources = (html: string, baseUrl: string, resourcesDirPath: string) => {
   const meta = getMeta(html, baseUrl, resourcesDirPath);
   const tasks = new Listr(meta.resources.map(({ url, outputPath }) => (
     {
@@ -28,10 +28,10 @@ const downloadResources = (html, baseUrl, resourcesDirPath) => {
   return fsp.mkdir(resourcesDirPath).then(() => tasks.run()).then(() => meta.html);
 };
 
-const savePage = (htmlPath, html) => prettifyHTML(html)
+const savePage = (htmlPath: string, html: string) => prettifyHTML(html)
   .then((data) => saveFile(htmlPath, data));
 
-export default (pageUrl, outputDir = process.cwd()) => {
+export default (pageUrl: string, outputDir = process.cwd()) => {
   const { origin: baseUrl, hostname, pathname } = new URL(pageUrl);
   const baseName = sanitizeFileName(buildPath(hostname, pathname));
   const htmlName = baseName.concat('.html');
